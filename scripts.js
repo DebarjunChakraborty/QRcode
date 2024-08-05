684f48be5b488abd9ef93c71d61ae3a8ed757179
Debarjun Chakraborty <147123894+DebarjunChakraborty@users.noreply.github.com>
2024-08-05 17:37:44 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function loadHomePage() {
+    window.location.href = "index.html";
+}
+
+function loadAdminPage() {
+    window.location.href = "admin.html";
+}
+
+function loadStudentPage() {
+    window.location.href = "student.html"; // Placeholder for student page
+}
